224f8701b2cddc18ab657d9bee5cab706eb145d8
Administrator <Administrator@DESKTOP-04JEO1E>
2018-08-25 13:49:27 +0800

    Hello World Demo

---
+++ ThreadDemo/src/com/wzd/demo/HelloWorld.java
@@ -0,0 +1,26 @@
+
+    /**  
+     * @Title: HelloWorld.java
+     * @Package com.wzd.demo
+     * @Description: HelloWorld 演示
+     * @author Administrator
+     * @date 2018年8月25日下午1:47:57
+     * @version V1.0  
+     */
+    
+package com.wzd.demo;
+
+
+    /**
+     * @ClassName: HelloWorld
+     * @Description: 演示GitHub
+     * @author Administrator
+     * @date 2018年8月25日下午1:47:57
+     */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+}
