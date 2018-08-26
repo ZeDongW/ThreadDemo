f868e83b289cfdf42c45238a5c94cc2ef0830b14
Administrator <Administrator@DESKTOP-04JEO1E>
2018-08-26 16:30:36 +0800

    简单死锁实例

---
+++ ThreadDemo/src/com/wzd/deadlock/DeadLock.java
@@ -0,0 +1,55 @@
+
+    /**  
+     * @Title: DeadLock.java
+     * @Package com.wzd.deadlock
+     * @Description: 简单死锁例子
+     * @author Administrator
+     * @date 2018年8月26日下午3:22:36
+     * @version V1.0  
+     */
+    
+package com.wzd.deadlock;
+
+
+    /**
+     * @ClassName: DeadLock
+     * @Description: 简单死锁例子
+     * @author Administrator
+     * @date 2018年8月26日下午3:22:36
+     */
+
+public class DeadLock implements Runnable{
+	
+	    /* (非 Javadoc)
+	    * 
+	    * 
+	    * @see java.lang.Runnable#run()
+	    */
+	    
+	@Override
+	public void run() {
+		if("A".equals(Thread.currentThread().getName())) {
+			synchronized ("A") {
+				System.out.println(Thread.currentThread().getName() + "   get A  wait B......");
+				synchronized ("B") {
+					System.out.println(Thread.currentThread().getName() + "   get A  and B, success!!");
+				}
+			}
+		} else if ("B".equals(Thread.currentThread().getName())) {
+			synchronized ("B") {
+				System.out.println(Thread.currentThread().getName() + "   get B  wait A......");
+				synchronized ("A") {
+					System.out.println(Thread.currentThread().getName() + "   get A  and B, success!!");
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		DeadLock dl = new DeadLock();
+		Thread t1 = new Thread(dl, "A");
+		Thread t2 = new Thread(dl, "B");
+		t1.start();
+		t2.start();
+	}
+}
